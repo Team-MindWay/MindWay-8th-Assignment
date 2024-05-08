b0d0684f8057ab9959d67355b4d5ddcef144b7d1
nahyunuk <s24022@gsm.hs.kr>
2024-05-08 20:20:02 +0900

    1

--- Homework1.kt
+++ Homework1.kt
@@ -1,4 +1,16 @@
+fun main() {
+  val word = "마인드웨이" //word[0] = 마 word[1] 인  word[2] = 드  word[3] = 웨  word[4] = 이
+  val length = word.length
 
-fun main(args : Array<String>) {
-  println("Hello, World!")
-}
+  for (i in 0 until length) { //변수 i를 변수 length부터 0까지 반복
+      var output = "" // 변수 output을 선언 및 초기화
+      for (j in i until length) { //변수 j을 i까지 반복
+          output += word[j]// output에 word의 j번쨰 글자를 저장
+      }
+      for (j in 0 until i) // 변수 j가 0부터 i까지 반복
+      {
+          output += word[j]// output에 word의 j번째 글자를 저장
+      }
+      println(output)//output을 출력
+  }
+}  
---
+++ Homework2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+void organize(char str[][51], int compare);
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    char book_name[1000][51];
+    int counting[1000] = { 0 }; 
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s", book_name[i]); 
+    }
+
+    organize(book_name, n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j <= n; j++) {
+            if (strcmp(book_name[i], book_name[j]) == 0) { 
+                counting[i]++;
+            }
+        }
+    }
+
+    int max = 0;
+    for (int i = 1; i < n; i++) {
+        if (counting[i] > counting[max]) {
+            max = i;
+        }
+    }
+
+    printf("%s\n", book_name[max]);
+    return 0;
+}
+
+void organize(char str[][51], int compare) {
+    char temp[51];
+    for (int i = 0; i < compare; i++) {
+        for (int j = i+1; j < compare; j++) {
+            if (strcmp(str[i], str[j]) > 0) {
+                strcpy(temp, str[i]);
+                strcpy(str[i], str[j]);
+                strcpy(str[j], temp);
+            }
+        }
+    }
+}
