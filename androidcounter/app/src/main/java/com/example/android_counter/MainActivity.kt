21cda59e892afad9c7e51b16da4f04f5272ec5bb
nahyunuk <s24022@gsm.hs.kr>
2024-06-24 22:24:20 +0900

    fix : "코드 스타일 변경"

--- androidcounter/app/src/main/java/com/example/android_counter/MainActivity.kt
+++ androidcounter/app/src/main/java/com/example/android_counter/MainActivity.kt
@@ -1,26 +1,29 @@
 package com.example.android_counter
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.android_counter.databinding.ActivityMainBinding
+import java.io.Closeable
 
-class MainActivity : AppCompatActivity() {
 
-    fun main(){
-        setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.bt_cut)
-        val num = findViewById<TextView>(R.id.tv_num)
-        var add :Int = 0
-        button.setOnClickListener(){
-            add++
-            num.text = add.toString()
-        }
-    }
 
+class   MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
+    var add: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        main()
+    binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btCut.setOnClickListener{
+            add++
+            binding.tvNum.text = add.toString()
+
+        }
     }
 }
