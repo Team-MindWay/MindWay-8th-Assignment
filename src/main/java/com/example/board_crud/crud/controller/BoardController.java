007626e8d9ea3eede9fc62dbe1608556cd76a087
김승찬 <s24041@gsm.hs.kr>
2024-07-24 19:28:35 +0900

    fix : "use RequestMapping"

--- src/main/java/com/example/board_crud/crud/controller/BoardController.java
+++ src/main/java/com/example/board_crud/crud/controller/BoardController.java
@@ -5,30 +5,30 @@ import com.example.board_crud.crud.Dto.BoardRequestDto;
 import com.example.board_crud.crud.Dto.BoardResponseDto;
 import com.example.board_crud.crud.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/boards")
 public class BoardController {
 
     private final BoardService boardService;
 
     //글 등록
-    @PostMapping("/boards")
+    @PostMapping
     public BoardResponseDto createBoard(@RequestBody BoardRequestDto requestDto){
         BoardResponseDto board = boardService.createBoard(requestDto);
         return board;
     }
     //전체 목록 조회
-    @GetMapping("/boards")
+    @GetMapping
     public List<BoardListResponseDto> getAllBoards(){
         return boardService.findAllBoard();
     }
     //글 하나 조회
-    @GetMapping("/boards/{id}")
+    @GetMapping("/{id}")
     public BoardResponseDto getOneBoard(@PathVariable Long id){
         return boardService.findOneBoard(id);
     }
@@ -38,12 +38,12 @@ public class BoardController {
         return boardService.updateBoard(id, requestDto);
     }
     //글 삭제
-    @DeleteMapping("/boards/{id}")
+    @DeleteMapping("/{id}")
     public Long deleteBoard(@PathVariable Long id){
         return boardService.deleteBoard(id);
     }
     //비밀번호 확인
-    @GetMapping("boards/check/{id}/{inputPassword}")
+    @GetMapping("/check/{id}/{inputPassword}")
     public boolean checkPassword(@PathVariable Long id,@PathVariable String inputPassword){
         return boardService.checkPassword(id, inputPassword);
     }
